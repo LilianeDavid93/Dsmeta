3738141cb2a7196c2206a56b8242161157b8dbc1
Liliane David Joia <lilianemarquesdavid@gmail.com>
2022-11-14 23:16:44 -0300

    Desenvolvimento layout dsmeta

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
-
-
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 function App() {
-  return(
-    <h1>Olá mundo</h1>
-  )
+  return (
+    <>
+      <Header/>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/notificationButton/index.tsx
@@ -0,0 +1,12 @@
+import Icon from '../../assets/img/icone1.svg';
+
+function NotificationButton() {
+  return (
+    <div className="dsmeta-red-btn">
+          <img src={Icon} alt="Notificar" />
+    </div>
+  )
+}
+
+export default NotificationButton;
+

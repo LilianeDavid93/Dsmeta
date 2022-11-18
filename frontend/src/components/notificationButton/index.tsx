69800798f48534372c26e85bd17558c113d46346
Liliane David Joia <lilianemarquesdavid@gmail.com>
2022-11-18 19:01:48 -0300

    Toast

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -2,6 +2,7 @@ import Icon from '../../assets/img/icone1.svg';
 import axios from "axios";
 import "./style.css";
 import { BASE_URL } from "../../utils/request"
+import { toast } from 'react-toastify';
 
 
 type Props = {
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id :number){
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response =>{
-    console.log("sucesso")
+    toast.info("SMS Enviado com sucesso");
   })
 }
 

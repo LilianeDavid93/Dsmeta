f5dd6e15d9859932aec12e876034dbc16dffd927
Liliane David Joia <lilianemarquesdavid@gmail.com>
2022-11-18 18:54:51 -0300

    SendNotification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,12 +1,32 @@
 import Icon from '../../assets/img/icone1.svg';
+import axios from "axios";
+import "./style.css";
+import { BASE_URL } from "../../utils/request"
 
-function NotificationButton() {
+
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id :number){
+  axios(`${BASE_URL}/sales/${id}/notification`)
+  .then(response =>{
+    console.log("sucesso")
+  })
+}
+
+
+function NotificationButton({saleId}: Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
           <img src={Icon} alt="Notificar" />
     </div>
   )
 }
 
 export default NotificationButton;
+
+
+
+
 
